2e07db2ea97d9657cae9c1977194969b303cdb1e
Prometheus2677 <150411645+Prometheus2677@users.noreply.github.com>
2025-05-01 11:40:50 -0400

    feat: update dynamic time

--- email_tracking.js
+++ email_tracking.js
@@ -246,7 +246,7 @@ function fetchEmailsForCertainDay() {
 
   const [month, day, year] = targetDate.split("/").map(Number);
 
-  const endET = new Date(`${month}/${day}/${year} 18:${today.getMinutes()}:${today.getSeconds()} GMT-0400`);
+  const endET = new Date(`${month}/${day}/${year} ${today.getHours()}:${today.getMinutes()}:${today.getSeconds()} GMT-0400`);
   const startET = new Date(endET.getTime() - 20 * 60 * 60 * 1000);
 
   const time1 = Math.floor(startET.getTime() / 1000);
